68415f367f39bba08ea4f2350b9df2dcc4041454
Deray <33969267+LOoLzeC@users.noreply.github.com>
2019-02-11 18:07:00 +0700

    Add files via upload

---
+++ data/color.py
@@ -0,0 +1,13 @@
+# Manual Color
+
+W = '\033[1;37m' 
+N = '\033[0m'
+R = '\033[31m'
+B = '\033[1;37m\033[34m' 
+G = '\033[1;32m'
+O = '\033[33m'
+C = '\033[36m'
+notice  = "{}{}[*]{} ".format(N,B,N)
+warning = "{}[-]{} ".format(R,N)
+good    = "{}[!]{} ".format(G,N)
+warn    = "{}[!]{} ".format(O,N)
